39a4c33542013c2c94491dea6d8ffd7714b933aa
Guilherme Alves <ghfa_9@hotmail.com>
2019-08-16 17:44:58 -0300

    Add files via upload

---
+++ testeSpotify.js
@@ -0,0 +1,52 @@
+// OBTER NOME DA MÚSICA E DOS ARTISTAS DE UMA PLAYLIST DO SPOTIFY WEB
+// CONSOLE.SAVE: http://bgrins.github.io/devtools-snippets/#console-save
+// SOURCES > SNIPPETS > NEW SNIPPET > consoleSave.js > save > run
+
+function nomeMusicas(qtd) {
+    arrMusica = [];
+    arrArtista = [];
+    arrCompleto = [];
+    for(let i=0; i<=qtd; i++) {
+        musicas = document.getElementsByClassName('tracklist-name');
+        artistas = document.getElementsByClassName('TrackListRow__artists');
+        arrMusica.push(musicas[i].innerText);
+        arrArtista.push(artistas[i].innerText);
+        arrCompleto.push(musicas[i].innerText + ' - ' + artistas[i].innerText);
+    }
+    // console.log(arrMusica);
+    // console.log(arrArtista);
+    console.log(arrCompleto);
+    console.save(arrCompleto);
+}
+
+/* CONSOLE SAVE
+
+
+(function(console){
+
+console.save = function(data, filename){
+
+    if(!data) {
+        console.error('Console.save: No data')
+        return;
+    }
+
+    if(!filename) filename = 'console.json'
+
+    if(typeof data === "object"){
+        data = JSON.stringify(data, undefined, 4)
+    }
+
+    var blob = new Blob([data], {type: 'text/json'}),
+        e    = document.createEvent('MouseEvents'),
+        a    = document.createElement('a')
+
+    a.download = filename
+    a.href = window.URL.createObjectURL(blob)
+    a.dataset.downloadurl =  ['text/json', a.download, a.href].join(':')
+    e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
+    a.dispatchEvent(e)
+ }
+})(console)
+
+*/
